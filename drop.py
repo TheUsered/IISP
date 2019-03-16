ffd99c13c695933f9a54583e0ac0ab6a20212b22
TheUsered <44713728+TheUsered@users.noreply.github.com>
2019-03-16 17:29:13 -0400

    Delete drop.py

--- drop.py
+++
@@ -1,5 +0,0 @@
-#imagehouse = PhotoImage(file="/home/evan/Documents/IISPImages/house.gif")
-'''imagepark = PhotoImage(file="/home/evan/Documents/IISPImages/")
-imageoffice = PhotoImage(file="/home/evan/Documents/IISPImages/")'''
-
-
